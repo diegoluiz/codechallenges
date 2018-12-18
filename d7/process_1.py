e4a1c4600e8c6be2d08b2b1ae77701e38804b8b3
Diego Luiz <diegorcl@gmail.com>
2018-12-18 00:19:09 +0000

    added day 7

--- d7/process_1.py
+++ d7/process_1.py
@@ -1,4 +1,3 @@
-file = 'd7/input.test.txt'
 file = 'd7/input.txt'
 with open(file) as f:
   input = f.read().splitlines()
@@ -8,37 +7,22 @@ for item in input:
   a = item.replace('Step ','').replace(' can begin.','').split(' must be finished before step ')
   steps.append({ 'from': a[0], 'to': a[1] })
 
-# for i in steps: print(i)
-
-froms = list(filter(lambda x: x not in map(lambda x: x['to'], steps), set(map(lambda x: x['from'], steps))))
-
-to_process = set(froms)
+to_process = set(list(filter(lambda x: x not in map(lambda x: x['to'], steps), set(map(lambda x: x['from'], steps)))))
 chain = ''
-# to_process.add(unique_from)
-c = 0  
 
 def get_next(chain, to_process):
   for c in sorted(to_process):
     deps = list(filter(lambda x: x['to'] == c, steps))
     len_unprocessed_deps = len(list(filter(lambda x: x['from'] not in chain, deps)))
-    # print(' - deps', c, deps, len_unprocessed_deps)
     if len_unprocessed_deps == 0: return c
-    
-  # n = (sorted(to_process))[0]
 
 while len(to_process) > 0:
-  # n = (sorted(to_process))[0]
   n = get_next(chain, to_process)
-  print('processing', n)
   to_process.remove(n)
-  # print(to_process)
 
   for i in filter(lambda x: x['from'] == n, steps):
-    # print(' - ', str(i))
     to_process.add(i['to'])
 
   chain += n
-  print(to_process)
-  print('-' * 30)
 
 print(chain)
