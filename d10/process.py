213029fd08e764bb89fafb89fbc2d70fd0ada407
Diego Luiz <diegorcl@gmail.com>
2018-12-21 07:06:26 +0000

    finished day 10

--- d10/process.py
+++ d10/process.py
@@ -1,3 +1,111 @@
-file = 'd10/input.txt'
-with open(file) as f:
-  input = f.read().splitlines()
+
+class Star:
+  def __init__(self, id, input):
+    self.id = id
+    self.i_x = int(input[0].split(', ')[0])
+    self.i_y = int(input[0].split(', ')[1])
+    self.v_x = int(input[1].split(', ')[0])
+    self.v_y = int(input[1].split(', ')[1])
+
+    self.x = self.i_x
+    self.y = self.i_y
+  
+  def init_print(self):
+    return "Id: " + str(self.id) + " POS:" + str(self.x) + ','+ str(self.y)+ " VEL:" + str(self.v_x) + ','+ str(self.v_y)
+
+  def print(self):
+    return "Id: " + str(self.id) + " POS:" + str(self.x) + ','+ str(self.y)
+
+def get_input():
+  file = 'd10/input.test.txt'
+  file = 'd10/input.txt'
+  with open(file) as f:
+    input = []
+    c = 0
+    for i in f.read().splitlines():
+      temp = i.replace('position', '') \
+        .replace('=<', '') \
+        .replace('>', '') \
+        .split(' velocity')
+      input.append(Star(c, temp))
+      c += 1
+  return input
+
+def get_bounderies(stars):
+  line_x = sorted([i.x for i in stars ])
+  line_y = sorted([i.y for i in stars ])
+  left = line_x[0]
+  right = line_x[-1]
+  bottom = line_y[0]
+  top = line_y[-1]
+
+  return top, left, bottom, right
+
+def new_grid(stars, top, left, bottom, right):
+  grid = []
+  for i in range(bottom, top  + 1):
+    grid.append(['.'] * (right - left + 1))
+
+  return grid, (left * -1), (bottom * -1)
+
+def validate(grid):
+  for x in range(0, len(grid[0])):
+    for y in range(0, len(grid) - 5):
+      count = 0
+      for i in range(0, 5):
+        if grid[y+i][x] == '#': 
+          count += 1
+      if count >= 5: 
+        return True
+  return False
+
+def print_grid(grid):
+  for row in grid:
+    for i in row: print(i, end='')
+    print('')
+  print('*' * 50)
+
+stars = get_input()
+for i in stars: print(i.init_print())
+
+total_seconds = 100000
+max_y = 1000
+max_x = 1000
+grid = None
+for second in range(0, total_seconds):
+  top, left, bottom, right = get_bounderies(stars)
+  print('Another second', second, 'bounduaries', top,left,bottom,right)
+
+  if abs(top - bottom) <= max_y and abs(left - right) <= max_x:
+    grid, x_offset, y_offset = new_grid(stars, top, left, bottom, right)
+    print(' - grid created', x_offset, y_offset)
+
+  is_valid = False
+  for star in stars:
+    if (grid is not None): 
+      grid[star.y + y_offset][star.x + x_offset] = '#'
+
+    star.x += star.v_x
+    star.y += star.v_y
+  
+  # print(' - Done all movements')
+
+  if (grid is not None) and validate(grid):
+    print_grid(grid)
+
+  grid = None
+
+# Another second 10932 bounduaries 145 106 136 167
+#  - grid created -106 -136
+# #....#..######..#....#...####...#####......###...####...######
+# #...#........#..#....#..#....#..#....#......#...#....#.......#
+# #..#.........#..#....#..#.......#....#......#...#............#
+# #.#.........#...#....#..#.......#....#......#...#...........#.
+# ##.........#....######..#.......#####.......#...#..........#..
+# ##........#.....#....#..#..###..#..#........#...#..###....#...
+# #.#......#......#....#..#....#..#...#.......#...#....#...#....
+# #..#....#.......#....#..#....#..#...#...#...#...#....#..#.....
+# #...#...#.......#....#..#...##..#....#..#...#...#...##..#.....
+# #....#..######..#....#...###.#..#....#...###.....###.#..######
+
+# KZHGRJGZ
--- d11/process.py
+++ d11/process.py
@@ -1,3 +1,50 @@
-file = 'd11/input.txt'
-with open(file) as f:
-  input = f.read().splitlines()
+# input = 39
+# input = 71
+input = 8444
+
+debug = False
+grid = []
+for y in range(0, 300):
+  row = []
+  grid.append(row)
+  for x in range(0, 300):
+    g_y = y + 1
+    g_x = x + 1
+    # if g_y == 196 and g_x == 217:
+    #   debug = True
+    rack_id = g_x + 10
+    power_level = rack_id * g_y
+    power_level += input
+    power_level *= rack_id
+    
+    power_level_str = str(power_level).rjust(3,'0')
+    power_level_str = "".join(reversed(power_level_str))
+
+    level = int(power_level_str[2]) - 5
+    # print(level, end=' ')
+    row.append(level)
+  # print('')
+  # print(row)
+
+# print(grid[152][100])
+# print(grid[216][196])
+
+# for y in grid:
+#   for x in y:
+#     print(x, end='')
+#   print('')
+
+max_total = 0
+max_y = 0
+max_x = 0
+for y in range(0, 298):
+  for x in range(0, 298):
+    total = grid[y][x] + grid[y][x + 1] + grid[y][x + 2] \
+          + grid[y + 1][x] + + grid[y + 1][x + 1] + grid[y+1][x + 2] \
+          + grid[y + 2][x] + + grid[y + 2][x + 1] + grid[y + 2][x + 2]
+    if total >= max_total:
+      max_total = total
+      max_x = x
+      max_y = y
+
+print(max_total, max_x + 1, max_y + 1)
