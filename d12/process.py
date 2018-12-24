c7811c2c70fd94eec182490cd88fc6f7c4d477ce
Diego Luiz <diegorcl@gmail.com>
2018-12-24 11:43:35 +0000

    wip day 12

--- d12/process.py
+++ d12/process.py
@@ -1,3 +1,32 @@
-file = 'd12/input.txt'
+file = 'd12/input.test.txt'
+# file = 'd12/input.txt'
+
+
+def parse_rule(i):
+  x = i.split(' => ')
+  return {'input': x[0], 'output': x[1]}
+
+def change_state(state, zero_pos):
+  for i in range(5):
+    if state[i] == '#':
+      state = '.' + state
+      zero_pos += 1
+
+    if state[0 - i - 1] == '#':
+      state = state + '.'
+
+  return state, zero_pos
+
 with open(file) as f:
   input = f.read().splitlines()
+
+zero_pos = 0
+initial_state = input[0].replace('initial state: ', '')
+rules = [parse_rule(i) for i in input[2:]]
+
+state, zero_pos = change_state(initial_state, zero_pos)
+
+
+print('init',initial_state)
+print('state',state, zero_pos)
+for i in rules: print(i)
