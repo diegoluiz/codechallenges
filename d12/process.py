0381707184ea986aa6948b3747c76e0adc30e08e
Diego Luiz <diegorcl@gmail.com>
2018-12-25 21:47:33 +0000

    day 12 wip

--- d12/process.py
+++ d12/process.py
@@ -4,7 +4,10 @@ file = 'd12/input.test.txt'
 
 def parse_rule(i):
   x = i.split(' => ')
-  return {'input': x[0], 'output': x[1]}
+  i = x[0]
+  o = x[0][0:2] + x[1] + x[0][3:5]
+  o = '..' + x[1] + '..'
+  return {'input': i, 'output': o, 'r': x[1]}
 
 def change_state(state, zero_pos):
   for i in range(5):
@@ -24,9 +27,32 @@ zero_pos = 0
 initial_state = input[0].replace('initial state: ', '')
 rules = [parse_rule(i) for i in input[2:]]
 
-state, zero_pos = change_state(initial_state, zero_pos)
-
-
+# state, zero_pos = change_state(initial_state, zero_pos)
 print('init',initial_state)
-print('state',state, zero_pos)
 for i in rules: print(i)
+
+for i in range(0, 1):
+  state, zero_pos = change_state(initial_state, zero_pos)
+  temp_state = state
+
+  print(state, zero_pos)
+  for i in range(0 - zero_pos, len(state)):
+    print(str(i)[-1], end='')
+  print()
+  
+  # last_rule = -1
+  for rule in rules:
+    idx = temp_state.find(rule['input'])
+    while idx != -1:
+    # idx = state.find(rule['input'])
+    # if (idx >= 0):
+      last_rule = idx
+      print(idx + 1, rule['input'], temp_state)
+      temp_state = temp_state[0: idx] + rule['output'] + temp_state[idx + 5:]
+      idx = temp_state.find(rule['input'])
+
+      # temp_state
+    # break
+  print(temp_state, zero_pos)
+
+
