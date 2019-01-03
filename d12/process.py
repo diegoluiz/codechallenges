dcc87e799e646414a96acea06e56ad8c20cfbecb
Diego Luiz <diegorcl@gmail.com>
2019-01-03 19:29:38 +0000

    passed day 12

--- d12/process.py
+++ d12/process.py
@@ -31,7 +31,8 @@ print('init',initial_state)
 for i in rules: print(i)
 
 state = initial_state
-for tick in range(0, 20):
+# for tick in range(0, 20):
+for tick in range(0, 100):
   state, zero_pos = change_state(state, zero_pos)
   temp_state = state
   for pos in range(0, len(state) - 2):
@@ -44,6 +45,8 @@ for tick in range(0, 20):
 
   state = temp_state
   print(tick + 1,':', state, zero_pos)
+  # if tick % 500 == 0:
+  #   print(tick, len(state))
 
 print()
 print('final', state)
@@ -53,5 +56,29 @@ total = 0
 for p in range(0, len(state)):
   if state[p] == '#':
     total += (p - zero_pos)
+    print(p - zero_pos)
 
 print ('total', total)
+
+
+# it's impossible to calculate 50 billion. So I ran until I found a pattern which doesn change, but moves around. 
+#  once i found it (tick 90, but to calculate I used tick 100), I got all the positions and added the (5 * 10^15) - 100 
+# 	50000000000	49999999900
+# 83	49999999983	
+# 84	49999999984	
+# 91	49999999991	
+# 92	49999999992	
+# 154	50000000054	
+# 155	50000000055	
+# 157	50000000057	
+# 159	50000000059	
+# 162	50000000062	
+# 163	50000000063	
+# 176	50000000076	
+# 177	50000000077	
+# 179	50000000079	
+# 182	50000000082	
+# 183	50000000083	
+		
+		
+# 	750000000697	
