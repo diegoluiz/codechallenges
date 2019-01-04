5a736d02c3f10b14a6b467e7ea60d6fc2a94ef7a
Diego Luiz <diegorcl@gmail.com>
2019-01-04 08:52:27 +0000

    started d13

--- d13/process.py
+++ d13/process.py
@@ -1,3 +1,11 @@
-file = 'd13/input.txt'
+file = 'd13/input.test.txt'
+# file = 'd13/input.txt'
+
 with open(file) as f:
   input = f.read().splitlines()
+  input_map = [list(row) for row in input]
+
+raw_map = input_map
+
+for row in raw_map:
+  print(row)
