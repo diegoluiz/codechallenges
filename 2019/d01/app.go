c0858a33375aabdf642e9449b2345c7d880d5ff0
Diego Luiz <diegorcl@gmail.com>
2019-12-04 08:39:28 +0000

    d01 - p2

--- 2019/d01/app.go
+++ 2019/d01/app.go
@@ -11,18 +11,35 @@ func main() {
 	fmt.Printf("day 01\n")
 	lines := readfile("input.txt")
 
+	totalFuel := 0
 	totalModulesFuel := 0
 	for _, line := range lines {
 		mass, _ := strconv.Atoi(line)
-		fuel := calculateMass(mass)
+		fuel := calculateFuel(mass)
+
+		fuelForFuel := calculateFuelRec(fuel)
 
 		totalModulesFuel += fuel
+		totalFuel += fuel
+		totalFuel += fuelForFuel
 	}
 	fmt.Printf("p1\n")
 	fmt.Println(totalModulesFuel)
+	fmt.Printf("\np2\n")
+	fmt.Println(totalFuel)
 }
 
-func calculateMass(mass int) int {
+func calculateFuelRec(mass int) int {
+	fuel := calculateFuel(mass)
+	if fuel > 0 {
+		fuel += calculateFuelRec(fuel)
+		return fuel
+	}
+
+	return 0
+}
+
+func calculateFuel(mass int) int {
 	v1 := mass / 3
 	v2 := int(v1)
 	v3 := v2 - 2
@@ -31,6 +48,7 @@ func calculateMass(mass int) int {
 }
 
 func readfile(file string) []string {
+	// return []string{"14", "1969"}
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Println("File reading error", err)
