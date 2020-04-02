26acfd760ef8623beb76fb2d5af6f33c11c947d5
Diego Luiz <diegorcl@gmail.com>
2020-04-02 19:36:27 +0100

    Added 30 days challenge - Day 02

---
+++ day-02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day_02
+{
+    class Program
+    {
+        public static bool IsHappy(int n)
+        {
+            var s = new HashSet<int> { };
+            while (!s.Contains(n))
+            {
+                if (n == 1) return true;
+
+                s.Add(n);
+                
+                var m = n;
+                n = 0;
+                while (m != 0)
+                {
+                    var t = m % 10;
+                    n += t * t;
+                    m /= 10;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            for (var n = 0; n < 100; n++)
+            {
+                Console.WriteLine($"Is {n} happy? {IsHappy(n)}".ToLower());
+            }
+        }
+    }
+}
