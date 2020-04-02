8d2096d9efeeb13fde14beef6d10b2d210abe8db
Diego Luiz <diegorcl@gmail.com>
2020-04-02 19:43:22 +0100

    Added 30 days challenge - Day 01

---
+++ day-01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day_01
+{
+    class Program
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            var hashMap = new Dictionary<int, int>();
+            foreach (var i in nums)
+            {
+                if (!hashMap.ContainsKey(i))
+                {
+                    hashMap.Add(i, 0);
+                }
+
+                hashMap[i] += 1;
+            }
+
+            foreach (var i in hashMap)
+            {
+                if (i.Value == 1)
+                {
+                    return i.Key;
+                }
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            var o = SingleNumber(new int[] { 4, 1, 2, 1, 2 });
+            Console.WriteLine($"{o}");
+        }
+    }
+}
